e4e09ee6862df33fb0b29e33cc5bc3666874aec5
Steimel <trs3ww@virginia.edu>
2014-04-17 23:27:37 -0400

    made some changes

--- parser.hs
+++ parser.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
@@ -6,12 +7,45 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+	
+data LispVal = Atom String
+		| List [LispVal]
+		| DottedList [LispVal] LispVal
+		| Number Integer
+		| String String
+		| Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+            char '"'
+            x <- many (noneOf "\"")
+            char '"'
+            return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do 
+                first <- letter <|> symbol
+                rest <- many (letter <|> digit <|> symbol)
+                let atom = first:rest
+                return $ case atom of 
+                           "#t" -> Bool True
+                           "#f" -> Bool False
+                           _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
 
 readExpr :: String -> String
-readExpr input = case parse (space >> symbol) "lisp" input of
-	Left err -> "No match: " ++ show err
-	Right val -> "Found value"
-	
+readExpr input = case parse parseExpr "lisp" input of
+     Left err -> "No match: " ++ show err
+     Right _ -> "Found value"
+
+		
 main :: IO ()
 main = do 
 	args <- getArgs
